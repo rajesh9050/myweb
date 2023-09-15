62d3abc47924aca18c1ba455067b8d1085613848
rajesh9050 <rajesh9050@gmail.com>
2023-09-15 07:08:48 +0530

    New Version of website

--- pages/Courses.py
+++ pages/Courses.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import streamlit.components.v1 as components
 st.set_page_config(
     page_title="Courses",
     page_icon="🧊",
@@ -7,4 +7,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 st.sidebar.header("Course Offered")
-st.title("Courses Comming soon....")
+st.title("Courses")
+components.html("<html><body><ul><li>Python</li><li>JAVA</li></ul></body></html>")
+
+
